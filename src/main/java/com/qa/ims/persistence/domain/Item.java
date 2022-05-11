d2c5ca90f7047fc4619d5c5d4c28dd25a6984913
Jehad <jehad.abdelbaqi@gmail.com>
2022-05-11 19:59:43 +0100

    JA: Added all item features to item features branch and commited

---
+++ src/main/java/com/qa/ims/persistence/domain/Item.java
@@ -0,0 +1,103 @@
+package com.qa.ims.persistence.domain;
+
+public class Item {
+	
+	private Long id;
+	private String itemName;
+	private Long itemPrice;
+	
+	
+	public Item(String itemName, Long itemPrice) {
+		this.setItemName(itemName);
+		this.setItemPrice(itemPrice);
+	}
+
+
+	public Item(Long id, String itemName, Long itemPrice) {
+		this.setId(id);
+		this.setItemName(itemName);
+		this.setItemPrice(itemPrice);
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getItemName() {
+		return itemName;
+	}
+
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+
+	public Long getItemPrice() {
+		return itemPrice;
+	}
+
+
+	public void setItemPrice(Long itemPrice) {
+		this.itemPrice = itemPrice;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Item [id=" + id + ", itemName=" + itemName + ", itemPrice=" + itemPrice + "]";
+	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((itemName == null) ? 0 : itemName.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((itemPrice == null) ? 0 : itemPrice.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Item other = (Item) obj;
+		if (getItemName() == null) {
+			if (other.getItemName() != null)
+				return false;
+		} else if (!getItemName().equals(other.getItemName()))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (itemPrice == null) {
+			if (other.itemPrice != null)
+				return false;
+		} else if (!itemPrice.equals(other.itemPrice))
+			return false;
+		return true;
+		
+//		return Objects.equals(id, other.id) && Objects.equals(itemName, other.itemName)
+//				&& Objects.equals(itemPrice, other.itemPrice);
+	}
+	
+	
+	
+	
+}
