65393a1cc71d5d20c21a1f07fc5c0f4ea4c481c0
Jehad <jehad.abdelbaqi@gmail.com>
2022-05-13 08:45:11 +0100

    JA: Commited all testing files

--- src/test/resources/sql-schema.sql
+++ src/test/resources/sql-schema.sql
@@ -6,3 +6,32 @@ CREATE TABLE IF NOT EXISTS `customers` (
     `surname` VARCHAR(40) DEFAULT NULL,
     PRIMARY KEY (`id`)
 );
+
+DROP TABLE IF EXISTS `items`;
+
+CREATE TABLE IF NOT EXISTS `items` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `item_name` VARCHAR(40) DEFAULT NULL,
+    `item_price` DOUBLE,
+    PRIMARY KEY (`id`)
+);
+
+DROP TABLE IF EXISTS `orders`;
+
+CREATE TABLE IF NOT EXISTS `orders` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `customer_id` INT(11) NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`customer_id`) REFERENCES `customers` (`id`)
+);
+
+DROP TABLE IF EXISTS `order_items`;
+
+CREATE TABLE IF NOT EXISTS `order_items`(
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`item_id` INT(11) NOT NULL,
+    `order_id` INT(11) NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`),
+    FOREIGN KEY (`item_id`) REFERENCES `items` (`id`)
+);
