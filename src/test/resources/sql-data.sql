a543a0363bc5c236e4c63a7d6aeea61c717c889a
Jehad <jehad.abdelbaqi@gmail.com>
2022-05-13 08:45:11 +0100

    JA: Commited all testing files

--- src/test/resources/sql-data.sql
+++ src/test/resources/sql-data.sql
@@ -1,1 +1,6 @@
 INSERT INTO `customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
+INSERT INTO `items` (`item_name`, `item_price`) VALUES ('Laptop', 999.99);
+INSERT INTO `orders` (`customer_id`) VALUES (1);
+INSERT INTO `orders` (`customer_id`) VALUES (2);
+INSERT INTO `order_items` (`item_id`, `order_id`) VALUES (1, 1);
+INSERT INTO `order_items` (`item_id`, `order_id`) VALUES (2, 2);
